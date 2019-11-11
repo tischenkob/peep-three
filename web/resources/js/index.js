f8536a7c3ad6688c74b86c29f74e52feb7e065fe
Bogdan <tischenkob@gmail.com>
2019-11-11 17:19:14 +0300

    formatted js

--- web/resources/js/index.js
+++ web/resources/js/index.js
@@ -1,60 +1,55 @@
-$(document).ready(function () {
-    $('#app-link').click(function () {
-        console.log("press app");
-    });
-    $('#info-button').click(function () {
-        console.log("press info");
-        $('#info-panel').classList.toggle("hidden");
-    });
-
-
+$(document).ready(function() {
+  $("#app-link").click(function() {
+    console.log("press app");
+  });
+  $("#info-button").click(function() {
+    console.log("press info");
+    $("#info-panel").classList.toggle("hidden");
+  });
 });
 
 function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
 
-    if (h == 0) {
-        h = 12;
-    }
+  if (h == 0) {
+    h = 12;
+  }
 
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
 
-    var today = new Date();
+  var today = new Date();
 
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
+  var day = today.getDate();
+  var month = today.getMonth() + 1;
+  var year = today.getFullYear();
 
-    if (day < 10) {
-        day = '0' + day
-    }
-    if (month < 10) {
-        month = '0' + month
-    }
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
 
-    var out = document.getElementById("date");
+  var out = document.getElementById("date");
 
-    out.innerHTML = day + "/" + month + "/" + year;
+  out.innerHTML = day + "/" + month + "/" + year;
 
-    setTimeout(showTime, 1000);
-
+  setTimeout(showTime, 1000);
 }
 
-
 showTime();
-
