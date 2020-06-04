9544a6f710d592f1fa2407090850e57a1f2f2d41
Bogdan <tischenkob@gmail.com>
2020-06-04 19:16:39 +0300

    stole checking logic from Borisenko

--- src/main/java/beans/AreaBean.java
+++ src/main/java/beans/AreaBean.java
@@ -3,6 +3,12 @@ package main.java.beans;
 public class AreaBean {
 
     public boolean checkHit(EntryBean bean) {
-        return false;
+        double xx = bean.getX();
+        double yy = bean.getY();
+        double rr = bean.getR();
+        boolean isHit = ((xx >= 0 && yy >= 0 && xx <= rr && yy <= rr)
+                || ((Math.pow(xx, 2) + Math.pow(yy, 2) <= Math.pow(rr / 2, 2)) && xx >= 0 && yy <= 0)
+                || (xx <= 0 && yy >= 0 && yy <= (xx + rr / 2)));
+        return isHit;
     }
 }
