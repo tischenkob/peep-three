4097343b3d93f455722735c91e793164107c89a7
Bogdan <tischenkob@gmail.com>
2020-06-05 14:09:38 +0300

    implemented the first test

--- src/test/AreaBeanTest.java
+++ src/test/AreaBeanTest.java
@@ -1,11 +1,18 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-public class AreaBeanTest {
-    @Test
-    public void testCheckHit() {
+import main.java.beans.AreaBean;
 
+public class AreaBeanTest {
+    AreaBean ab = new AreaBean();
+    double r = 2;
+
+    @Test
+    public void testCheckHit1() {
+        assertTrue(ab.checkHit(1, 1, r));
     }
 
 }
