c10ae851a1de520f14acd9d1549301ebddccfbb6
Bogdan <tischenkob@gmail.com>
2020-06-12 22:32:03 +0300

    a coupla new tests

--- src/test/AreaBeanTest.java
+++ src/test/AreaBeanTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,9 +11,36 @@ public class AreaBeanTest {
     AreaBean ab = new AreaBean();
     double r = 2;
 
+    // TRIANGLE TEST
     @Test
-    public void testCheckHit1() {
-        assertTrue(ab.checkHit(1, 1, r));
+    public void triangleCorner1() {
+        assertTrue(ab.checkHit(-1, 0, r));
     }
+
+    @Test
+    public void triangleCorner2() {
+        assertTrue(ab.checkHit(-0.999, 0.001, r));
+    }
+
+    @Test
+    public void testCheckHit2() {
+        assertTrue(ab.checkHit(-0.5, 0, r));
+    }
+
+    @Test
+    public void testCheckHit3() {
+        assertTrue(ab.checkHit(-0.5, 0, r));
+    }
+
+    @Test
+    public void testCheckHit4() {
+        assertFalse(ab.checkHit(-0.5, -0.0001, r));
+    }
+
+    // RECTANGLE TESTS
+
+    // CIRCLE TESTS
+
+    // EMPTY TESTS
 
 }
